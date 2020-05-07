adbf2aca38cd8a2c621278f79175812f005e8016
spoorthyvv <spoorthyvv@gmail.com>
2020-05-07 06:43:32 +0530

    My assignment

---
+++ array_demo.java
@@ -0,0 +1,23 @@
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]=new int[5];
+		arr[0]=10;
+		arr[1]=20;
+		arr[2]=30;
+		arr[3]=40;
+		arr[4]=50;
+		arr[5]=100;
+		System.out.println(arr[0]);
+		System.out.println(arr[1]);
+		System.out.println(arr[2]);
+		System.out.println(arr[3]);
+		System.out.println(arr[4]);
+		System.out.println(arr[5]);
+		//System.out.println(arr[0]);
+		
+	}
+
+}
+
